87fba7e25d5988e4c616713a5c1a6c33d7ffe610
David Hoze <david.hoze1@gmail.com>
2019-02-10 13:28:08 +0200

    Removed building of other libs

--- build.sh
+++ build.sh
@@ -8,18 +8,4 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-cd ../opus
-bash build.sh
-if [ $? -ne 0 ]; then
-	echo "Error: Build opus"
-	exit 1
-fi
-
-cd ../zip
-bash build.sh
-if [ $? -ne 0 ]; then
-	echo "Error: Build zip"
-	exit 1
-fi
-
 cd ..
